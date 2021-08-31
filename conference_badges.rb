7a93b4af518739f0e8df7269f2a9cd589e3d731e
Nancy Zaky <nancyzki@yahoo.com>
2021-08-30 23:22:45 -0700

    commit tree a0299bdf26c157ca2e73fe4d848734636b700814

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+def badge_maker(name)
+
+ "Hello, my name is #{name}."
+
+end
+
+def batch_badge_creator(arr)
+  newarr = []
+  arr.each do |item|
+    newarr <<  "Hello, my name is #{item}."
+  end
+  newarr
+end
+
+def assign_rooms(arr)
+  newarr = []
+  arr.each_with_index do |item, index|
+    newarr << "Hello, #{item}! You'll be assigned to room #{index+1}!"
+  end
+  newarr
+end
+
+def printer(arr)
+batch_badge_creator(arr).each do |item|
+  puts "#{item}"
+end
+assign_rooms(arr).each do |item|
+  puts "#{item}"
+end
+end
